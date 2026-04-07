03b448252be58792e35b0de390db6642f0da629a
jguevaraag <javitmdfe@gmail.com>
2026-04-07 15:33:22 +0200

    Finalmente intregado el backend y la base de datos a servidores online para poder iniciar las pruebas directamente desde ellos.

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -7,7 +7,7 @@ class ApiService {
   // Cambiar esta IP por la IP local del ordenador de Javi
   // Ej: Si Javi tiene la 192.168.1.55, pon 'http://192.168.1.55:8080/api'
   // Si usáis emulador Android apuntando al mismo PC, usad 'http://10.0.2.2:8080/api'
-  final String baseUrl = 'http://10.1.105.11:8080/api';
+  final String baseUrl = 'https://backend-focusup.onrender.com/api';
 
   ApiService() {
     dio = Dio(BaseOptions(
